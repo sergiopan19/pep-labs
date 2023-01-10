90cb4cc2ac3832949d3caf4e43af457c4164a939
sergiopan19 <sp19p@fsu.edu>
2023-01-10 01:37:12 +0000

    Saved progress on UncheckedException 2023-01-10 01:37:12 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,8 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
+        int arr[] = new int[1];
 
+        int i = arr[5];
     }
 }
