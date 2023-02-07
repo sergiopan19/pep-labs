927f3185fa56b347289f1d0a0c7ed671955519e9
sergiopan19 <sp19p@fsu.edu>
2023-02-07 01:26:56 +0000

    Saved progress on GetMax 2023-02-07 01:26:56 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,11 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = arr[0];
+
+        for(int i: arr)
+            if(i > max) max = i;
+
+        return max;
     }
 }
