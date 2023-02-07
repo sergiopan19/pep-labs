4aa09ef5ed26a3287039f3fbd4ee635d59b566f5
sergiopan19 <sp19p@fsu.edu>
2023-02-07 22:43:12 +0000

    Saved progress on CreateTable 2023-02-07 22:43:12 +0000

---
+++ CreateTable/problem1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE song(
+    title varchar(100) NOT NULL,
+    artist varchar(100) NOT NULL
+);
+
+INSERT INTO song (title, artist) VALUES
+
+('Let it be', 'Beatles'),
+('Hotel California', 'Eagles'),
+('Kashmir', 'Led Zepplin')
